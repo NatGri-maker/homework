38aafbbcce510bc0fa46bacfd0e9cb1560b2a85b
Nato Grigoriani <nato_grigoriani@iliauni.edu.ge>
2022-07-15 08:19:53 +0400

    created Routhes

--- MyReactProject/src/users/UserList.js
+++ MyReactProject/src/users/UserList.js
@@ -2,9 +2,33 @@ import {useEffect, useState} from "react";
 import axios from "axios";
 import {Button, Card, Col, Modal, Row} from "react-bootstrap";
 
+function UserCard({data, onOpen}) {
+    const {name, email, company, address} = data;
+
+    const handleClick = () => {
+        onOpen && onOpen(data);
+    };
+    return (
+        <Card>
+            <Card.Body>
+                <Card.Title>{name}</Card.Title>
+                <Card.Subtitle>{email}</Card.Subtitle>
+                <Card.Text>{address.city},{address.street}</Card.Text>
+                <Card.Text>{company.name}</Card.Text>
+            </Card.Body>
+            <Card.Footer>
+                <Button onClick={handleClick}>Open</Button>
+            </Card.Footer>
+        </Card>
+    );
+}
+
 function UserList() {
     const [users, setUsers] = useState([]);
     const [show, setShow] = useState(false);
+    const [currentUser, setCurrentUser] = useState({});
+    const [userPosts, setUserPosts] = useState([]);
+
 
     useEffect(() => {
         axios.get('https://jsonplaceholder.typicode.com/users')
@@ -13,35 +37,50 @@ function UserList() {
             .catch((error) => console.error(error));
     }, []);
 
-    const handleClose=()=>{
+    const handleClose = () => {
         setShow(false);
-    }
+    };
+
+    const handleUserOpen = async (userData) => {
+        setShow(true);
+        setCurrentUser(userData);
+        const {data} = await axios.get(`https://jsonplaceholder.typicode.com/posts`, {
+            params: {
+                userId: userData.id
+            }
+
+        });
+        setUserPosts(data);
+    };
     return (
-        <div className={"p-3"}>
+        <div className={"p-3 bg-secondary"}>
             <Row>
                 {
-                    users.map((user) =>(
+                    users.map((user) => (
                         <Col sm={6} md={4} lg={3} key={user.id} className={"mb-3"}>
-                            <Card>
-                                <Card.Body>
-                                    <Card.Title>{user.name}</Card.Title>
-                                    <Card.Subtitle>{user.email}</Card.Subtitle>
-                                   <Card.Text>{user.address.city},{user.address.street}</Card.Text>
-                                   <Card.Text>{user.company.name}</Card.Text>
-                                </Card.Body>
-                                <Card.Footer>
-                                    <Button onClick={}>Open</Button>
-                                </Card.Footer>
-                            </Card>
+                            <UserCard data={user} onOpen={handleUserOpen}/>
                         </Col>
                     ))
                 }
             </Row>
             <Modal show={show} onHide={handleClose} animation={false}>
                 <Modal.Header closeButton>
-                    <Modal.Title>Modal heading</Modal.Title>
+                    <Modal.Title>{currentUser.name}</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Body>{currentUser.email}
+                    {
+                        userPosts.map((post) => (
+
+                            <Card key={post.id}>
+                                <Card.Body>
+                                    <Card.Title>{post.title}</Card.Title>
+                                    <Card.Text>{post.body}</Card.Text>
+                                </Card.Body>
+                            </Card>
+                        ))
+                    }
+
+                </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}>
                         Close
